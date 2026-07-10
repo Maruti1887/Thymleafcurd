83fce610dfcc2fce6d1b0db24ce41bfff0493293
Maruti Hanumanth Naik <marutihanumanthnaik@gmail.com>
2026-07-10 14:00:44 +0530

    this is commit

--- src/main/java/com/example/thymleaf_crud2/controller/StudentController.java
+++ src/main/java/com/example/thymleaf_crud2/controller/StudentController.java
@@ -1,15 +1,10 @@
 package com.example.thymleaf_crud2.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -18,69 +13,72 @@ import com.example.thymleaf_crud2.dto.Student;
 
 @Controller
 public class StudentController {
-	@Autowired 
-	StudentDao studentDao;
 
-	@GetMapping("/")
+    @Autowired
+    private StudentDao studentDao;
+
+    // Redirect root URL to home page
+    @GetMapping("/")
     public String index() {
         return "redirect:/home";
     }
-	
-	@GetMapping("/home")
-	public String home() {
-		return "Studenthome.html";
-	}
-	
-	@GetMapping("/studentinsert")
-	public String sinsert() {
-		return "Studentinsert.html";
-	}
-	
-	@GetMapping("/fetchstu")
-	public String fetchids() {
-		return "Studentgeting.html";
-	}
-	
-	@GetMapping("/deletestu")
-	public String fetchall() {
-		return "Studentdelete.html";
-	}
-	
-	
-	@PostMapping("/inserting")
-	@ResponseBody
-	public String insert(@ModelAttribute Student student) {
-		return studentDao.insert(student);
-	}
-	
-	
-	
-	@GetMapping("/fetchid")
-	@ResponseBody
-	public Object fetchByid(@RequestParam int id) {
-		return studentDao.fetchByid(id);
-	}
-	
-	@GetMapping("/fetchalls")
-	@ResponseBody
-	public Object fetchbyid() {
-		return studentDao.fetchall();
-	}
-	
-	@GetMapping("/deleteid")
-	@ResponseBody
-	public String deltebyd(@RequestParam int id) {
-		return studentDao.deltebyd(id);
-	}
-	
-	@GetMapping("/deletealls")
-	@ResponseBody
-	public String deletell() {
-		return studentDao.deletell();
-		
-	}
 
-	
-	
+    // Home Page
+    @GetMapping("/home")
+    public String home() {
+        return "Studenthome";
+    }
 
+    // Student Insert Page
+    @GetMapping("/studentinsert")
+    public String studentInsert() {
+        return "Studentinsert";
+    }
+
+    // Fetch Student Page
+    @GetMapping("/fetchstu")
+    public String fetchStudent() {
+        return "Studentgeting";
+    }
+
+    // Delete Student Page
+    @GetMapping("/deletestu")
+    public String deleteStudent() {
+        return "Studentdelete";
+    }
+
+    // Insert Student
+    @PostMapping("/inserting")
+    @ResponseBody
+    public String insert(@ModelAttribute Student student) {
+        return studentDao.insert(student);
+    }
+
+    // Fetch Student by ID
+    @GetMapping("/fetchid")
+    @ResponseBody
+    public Object fetchById(@RequestParam int id) {
+        return studentDao.fetchByid(id);
+    }
+
+    // Fetch All Students
+    @GetMapping("/fetchalls")
+    @ResponseBody
+    public Object fetchAll() {
+        return studentDao.fetchall();
+    }
+
+    // Delete Student by ID
+    @GetMapping("/deleteid")
+    @ResponseBody
+    public String deleteById(@RequestParam int id) {
+        return studentDao.deltebyd(id);
+    }
+
+    // Delete All Students
+    @GetMapping("/deletealls")
+    @ResponseBody
+    public String deleteAll() {
+        return studentDao.deletell();
+    }
 }
